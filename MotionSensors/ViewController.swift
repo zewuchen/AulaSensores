d3c6e1ef678000c19e0e37339f106b1cd19f56de
Bruno Omella Mainieri <bruno.omella@hotmail.com>
2019-06-14 10:16:10 -0300

    Initial Commit

---
+++ MotionSensors/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MotionSensors
+//
+//  Created by Bruno Omella Mainieri on 14/06/19.
+//  Copyright © 2019 Bruno Omella Mainieri. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
