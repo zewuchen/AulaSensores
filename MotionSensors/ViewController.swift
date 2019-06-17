f608b6b079189b5624b985dfe3b8d1e1c84cbf58
Bruno Omella Mainieri <bruno.omella@hotmail.com>
2019-06-17 10:00:17 -0300

    Projeto base exemplificando uso de Attitude e Gravity

--- MotionSensors/ViewController.swift
+++ MotionSensors/ViewController.swift
@@ -7,13 +7,73 @@
 //
 
 import UIKit
+import CoreMotion
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var accX: UILabel!
+    @IBOutlet weak var accY: UILabel!
+    @IBOutlet weak var accZ: UILabel!
+    
+    
+    @IBOutlet weak var horizon: UIView!
+    
+    var referenceAttitude:CMAttitude?
+    
+    let motion = CMMotionManager()
+    
+    var lastXUpdate = 0
+    var lastYUpdate = 0
+    var lastZUpdate = 0
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        startDeviceMotion()
     }
+    
+    func startDeviceMotion() {
+        if motion.isDeviceMotionAvailable {
+            //Frequencia de atualização dos sensores definida em segundos - no caso, 60 vezes por segundo
+            self.motion.deviceMotionUpdateInterval = 1.0 / 60.0
+            self.motion.showsDeviceMovementDisplay = true
+            //A partir da chamada desta função, o objeto motion passa a conter valores atualizados dos sensores; o parâmetro representa a referência para cálculo de orientação do dispositivo
+            self.motion.startDeviceMotionUpdates(using: .xMagneticNorthZVertical)
+            
+            //Um Timer é configurado para executar um bloco de código 60 vezes por segundo - a mesma frequência das atualizações dos dados de sensores. Neste bloco manipulamos as informações mais recentes para atualizar a interface.
+            var timer = Timer(fire: Date(), interval: (1.0 / 60.0), repeats: true,
+                               block: { (timer) in
+                                if let data = self.motion.deviceMotion {
+                                    var relativeAttitude = data.attitude
+                                    if let ref = self.referenceAttitude{
+                                        //Esta função faz a orientação do dispositivo ser calculado com relação à orientação de referência passada
+                                        relativeAttitude.multiply(byInverseOf: ref)
+                                    }
+                                    
+                                    let x = relativeAttitude.pitch
+                                    let y = relativeAttitude.roll
+                                    let z = relativeAttitude.yaw
+                                    
+                                    self.accX.text = String(format: "%.3f", x)
+                                    self.accY.text = String(format: "%.3f", y)
+                                    self.accZ.text = String(format: "%.3f", z)
+                                    
+                                    let gravity = data.gravity
+                                    //Um pouco de matemágica para rotacionar o background de acordo com a orientação do dispositivo - neste caso, usando o vetor da gravidade para este cálculo
+                                    self.horizon.transform = CGAffineTransform(rotationAngle: CGFloat(atan2(gravity.x, gravity.y) - .pi))
+                                }
+            })
+            
+            RunLoop.current.add(timer, forMode: RunLoop.Mode.default)
+        }
+    }
+    
+    //Ao tocar na tela, a orientação atual do dispositivo passa a ser considerada a de referência com relação à qual os dados serão calculados
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        if let att = motion.deviceMotion?.attitude {
+            referenceAttitude = att
+        }
+    }
+    
+    
 
 
 }
